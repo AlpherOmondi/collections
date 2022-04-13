6122690fcca40279364d74c5bc9e0858b3753b41
AlphaOmondi <alphaomondi001@gmail.com>
2022-04-13 13:26:32 +0300

    First commit on lists, sets and maps

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+// Practising collections in kotlin- A group or related data e.g customer records
+// types of collections are - listOf,setOf,mapOf
+//listOf - contains duplicate items
+//toSet() converts
+//setOf -similar to listOf but doesn't contain duplicates
+//mapOf- Is like a dictionary, it pairs keys with their values
+// Functions to be used in manipulation of collections in kotlin are- sorted(),put()
+fun main() {
+    val identificationNums = listOf(90940,8956489,895895,675647,758748,40,40)
+    //printing the items in a list
+    println("The Ids are: ${identificationNums}")
+    //converting list to set
+    println("Converted list: ${identificationNums.toSet()}")
+    val age = setOf(1,3)
+    //Prints set of ages
+    println("The set of ages are:${age}")
+    val records = mapOf<String,Int>(
+        "Alpha" to 1,
+        "Felix" to 2,
+        "Purity" to 3,
+        "Amos" to 4
+    )
+    //print the map
+    println("The group members are : ${records}")
+    //Sorting the list
+    println("The sorted list is :${identificationNums.sorted()}")
+
+}
